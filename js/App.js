01eb2c46845a5d802aac9fd88d2ce7e39cffc427
maciekmacku <maciejszalaj@gmail.com>
2018-04-29 18:11:53 +0200

    fixed scripts

--- js/App.js
+++ js/App.js
@@ -1,11 +1,22 @@
+function randomString() {  //Main function
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+    var str = '',
+        i;
+    for (i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+// API variables
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
+  'X-Client-Id': '3207',
+  'X-Auth-Token': 'cb9844a6edaeaf0015d99887d7d80575'
 };
 $.ajaxSetup({
 	headers: myHeaders
 });
+
 $.ajax({
     url: baseUrl + '/board',
     method: 'GET',
@@ -13,30 +24,17 @@ $.ajax({
       setupColumns(response.columns);
     }
 });
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
 }
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	});
+}
--- js/Card.js
+++ js/Card.js
@@ -1,28 +1,34 @@
-// KLASA KANBAN CARD
-function Card(description) {
-	var self = this;
-	
-	this.id = randomString();
-	this.description = description;
-	this.element = createCard();
+// Kanban card class
+function Card(id, name) {
+    var self = this;
+    this.id = id;
+    this.name = name || 'No name given';
+    this.element = createCard();
 
-	function createCard() {
-		var card = $('<li class="card"></li>');
-		var cardDeleteBtn = $('<button class="btn-delete">x</button>');
-		var cardDescription = $('<p class="card-description"></p>');
-		
-		cardDeleteBtn.click(function(){
-			self.removeCard();
-		});
-		
-		card.append(cardDeleteBtn);
-		cardDescription.text(self.description);
-		card.append(cardDescription)
-		return card;
-	}
+    function createCard() {
+        var card = $('<li class="card"></li>');
+        var cardDeleteBtn = $('<button class="btn-delete">x</button>');
+        var cardDescription = $('<p class="card-description"></p>');
+
+        cardDeleteBtn.click(function () {
+            self.removeCard();
+        });
+
+        card.append(cardDeleteBtn);
+        cardDescription.text(self.name);
+        card.append(cardDescription);
+        return card;
+    }
 }
 Card.prototype = {
-	removeCard: function() {
-	  this.element.remove();
-	}
-}
+    removeCard: function () {
+        var self = this;
+        $.ajax({
+            url: baseUrl + '/card/' + self.id,
+            method: 'DELETE',
+            success: function () {
+                self.element.remove();
+            }
+        });
+    }
+};
